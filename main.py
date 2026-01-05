7aacf22d09f59162c5fa67f043a33ae73e9acde0
JiYueBo666 <3221349735@qq.com>
2026-01-05 21:51:41 +0800

    通配符

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import re
 
-s = "yuan rins 张三"
-print()
+s = "apple ace asde asxf ape bdaxe acce abaDe a.e a/e a?e"
+
+result = re.findall(pattern="a[^a-zA-Z]e", string=s)
+# a.e 匹配任意以a开头以e结尾。 通配符表任意
+print(result)  # ['ase', 'axe', 'ace']
